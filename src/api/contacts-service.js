bf03fb31e45e0c039b3d9104bbf1cebe5f065742
Borys Ziukov <ziukoff1985@gmail.com>
2025-12-12 12:49:27 +0200

    Fix server port

--- src/api/contacts-service.js
+++ src/api/contacts-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:5001/contacts',
+    baseURL: 'http://localhost:5000/contacts',
     headers: {
         'Content-Type': 'application/json',
     },
